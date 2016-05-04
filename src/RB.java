83b1846bfa3623ac1460470fc2ef6507b998414e
Thomas Brand <tom@trellis.ch>
2016-05-05 00:00:11 +0200

    update

--- src/RB.java
+++ src/RB.java
@@ -25,7 +25,7 @@ public class RB
 {
 	public static final byte RB_MAGIC[]={'r','i','n','g','b','u','f','\0'};
 	public static final String RB_MAGIC_STRING=new String(RB_MAGIC);
-	public static final float RB_VERSION=0.22f;
+	public static final float RB_VERSION=0.23f;
 	public static final String bar_string="============================================================";
 
 	//binary layout of C struct ( #pragma pack(1) to keep natural alignment )
@@ -36,26 +36,30 @@ public class RB
 	public static final int BOFF_last_was_write	=BOFF_write_index	+8;  //@  36: int  (4)
 	public static final int BOFF_memory_locked	=BOFF_last_was_write	+4;  //@  40: int  (4)
 	public static final int BOFF_in_shared_memory	=BOFF_memory_locked	+4;  //@  44: int  (4)
-	public static final int BOFF_unlink_requested	=BOFF_in_shared_memory	+4;  //@  48: int  (4)
-	public static final int BOFF_no_more_input_data	=BOFF_unlink_requested	+4;  //@  52: int  (4)
-	public static final int BOFF_sample_rate	=BOFF_no_more_input_data+4;  //@  56: int  (4)
-	public static final int BOFF_channel_count	=BOFF_sample_rate	+4;  //@  60: int  (4)
-	public static final int BOFF_bytes_per_sample	=BOFF_channel_count	+4;  //@  64: int  (4)
 
-	public static final int BOFF_total_bytes_read	=BOFF_bytes_per_sample	+4;  //@  68: long (8)
-	public static final int BOFF_total_bytes_write	=BOFF_total_bytes_read	+8;  //@  76: long (8)
-	public static final int BOFF_total_bytes_peek	=BOFF_total_bytes_write	+8;  //@  84: long (8)
-	public static final int BOFF_total_underflows	=BOFF_total_bytes_peek	+8;  //@  92: long (8)
-	public static final int BOFF_total_overflows	=BOFF_total_underflows	+8;  //@ 100: long (8)
+	public static final int BOFF_memory_lockable	=BOFF_in_shared_memory	+4;  //@  48: int  (4)
+	public static final int BOFF_memory_shareable	=BOFF_memory_lockable	+4;  //@  52: int  (4)
+	public static final int BOFF_mutex_lockable	=BOFF_memory_shareable	+4;  //@  56: int  (4)
 
-	public static final int BOFF_shm_handle		=BOFF_total_overflows	+8;  //@ 108: char [256]
-	public static final int BOFF_human_name		=BOFF_shm_handle	+256;//@ 364: char [256] (end 620)
+	public static final int BOFF_unlink_requested	=BOFF_mutex_lockable	+4;  //@  60: int  (4)
+	public static final int BOFF_no_more_input_data	=BOFF_unlink_requested	+4;  //@  64: int  (4)
+	public static final int BOFF_sample_rate	=BOFF_no_more_input_data+4;  //@  68: int  (4)
+	public static final int BOFF_channel_count	=BOFF_sample_rate	+4;  //@  72: int  (4)
+	public static final int BOFF_bytes_per_sample	=BOFF_channel_count	+4;  //@  76: int  (4)
 
+	public static final int BOFF_total_bytes_read	=BOFF_bytes_per_sample	+4;  //@  80: long (8)
+	public static final int BOFF_total_bytes_write	=BOFF_total_bytes_read	+8;  //@  88: long (8)
+	public static final int BOFF_total_bytes_peek	=BOFF_total_bytes_write	+8;  //@  96: long (8)
+	public static final int BOFF_total_underflows	=BOFF_total_bytes_peek	+8;  //@ 104: long (8)
+	public static final int BOFF_total_overflows	=BOFF_total_underflows	+8;  //@ 112: long (8)
+
+	public static final int BOFF_shm_handle		=BOFF_total_overflows	+8;  //@ 120: char [256]
+	public static final int BOFF_human_name		=BOFF_shm_handle	+256;//@ 376: char [256] (end 632 + 84)
+
+	//84 bytes mutex locks (optional, padded if not available, RB_DISABLE_RW_MUTEX)
 	//buffer data starts after the header
-	///private int header_length=BOFF_human_name+256          +84; ///wrong, excludes mutex
-	//RB_DISABLE_RW_MUTEX not set
-	//sizeof(rb_t): 704
-	private int header_length=704;
+	//sizeof(rb_t): 716
+	private int header_length=716;
 
 	//direct memory mapped bytebuffer, accessing c struct via RandomAccess file
 	private MappedByteBuffer mbb;
@@ -99,9 +103,14 @@ public class RB
 	public int last_was_write(){return mbb.getInt(BOFF_last_was_write);}
 	private void last_was_write(int was_write){mbb.putInt(BOFF_last_was_write,was_write);}
 
-	public int memory_locked(){return mbb.getInt(BOFF_memory_locked);}
-	public int in_shared_memory(){return mbb.getInt(BOFF_in_shared_memory);}
-	public int unlink_requested(){return mbb.getInt(BOFF_unlink_requested);}
+	public int is_memory_locked(){return mbb.getInt(BOFF_memory_locked);}
+	public int is_in_shared_memory(){return mbb.getInt(BOFF_in_shared_memory);}
+
+	public int is_memory_lockable(){return mbb.getInt(BOFF_memory_lockable);}
+	public int is_memory_shareable(){return mbb.getInt(BOFF_memory_shareable);}
+	public int is_mutex_lockable(){return mbb.getInt(BOFF_mutex_lockable);}
+
+	public int is_unlink_requested(){return mbb.getInt(BOFF_unlink_requested);}
 	public int no_more_input_data(){return mbb.getInt(BOFF_no_more_input_data);}
 	public int sample_rate(){return mbb.getInt(BOFF_sample_rate);}
 	public int channel_count(){return mbb.getInt(BOFF_channel_count);}
@@ -138,6 +147,31 @@ public class RB
 
 //=============================================================================
 	public RB(){}
+
+//=============================================================================
+	public long open_shared(String shm_handle) throws Exception
+	{
+		///more error checking needed
+		File f=new File(shm_handle);
+		if(!f.exists() || !f.canWrite())
+		{
+			throw new Exception("ringbuffer "+shm_handle+" not found or not writable.");
+		}
+		/*
+		"rws" Open for reading and writing, as with "rw", and also require that every 
+		update to the file's content or metadata be written synchronously to the 
+		underlying storage device. 
+		*/
+		RandomAccessFile in = new RandomAccessFile(f, "rws");
+		FileChannel fc = in.getChannel();
+
+		p("filechannel size bytes: "+fc.size()+" file size bytes "+f.length());
+
+		//map whole file
+		mbb=fc.map(FileChannel.MapMode.READ_WRITE,0,f.length());
+		mbb.order(ByteOrder.LITTLE_ENDIAN);
+		return fc.size();
+	}
 
 //=============================================================================
 	public long can_read()
@@ -200,13 +234,14 @@ public class RB
 			do_read_count=count > can_read_count ? can_read_count : count;
 		}
 
-		long linear_end=read_index()+do_read_count;
+		long r=read_index();
+		long linear_end=r+do_read_count;
 		long copy_count_1;
 		long copy_count_2;
 
 		if(linear_end>size())
 		{
-			copy_count_1=size()-read_index();
+			copy_count_1=size()-r;
 			copy_count_2=linear_end-size();
 		}
 		else
@@ -217,13 +252,13 @@ public class RB
 
 		//memcpy(destination, &( ((char*)buf_ptr(rb)) [rb->read_index] ), copy_count_1);
 		//position at current read_index
-		mbb.position((int)(header_length+read_index()));
+		mbb.position((int)(header_length+r));
 		//get(byte[] dst, int offset, int length)
 		mbb.get(destination, 0, (int)copy_count_1);
 
 		if(copy_count_2<1)
 		{
-			read_index( (read_index()+copy_count_1) % size() );
+			read_index( (r+copy_count_1) % size() );
 		}
 		else
 		{
@@ -248,7 +283,6 @@ public class RB
 
 		return do_read_count;
 	}//end generic_read
-
 
 //=============================================================================
 	public long peek(byte[] destination, long count)
@@ -319,29 +353,49 @@ public class RB
 	}//end peek_at
 
 //=============================================================================
-	public long open_shared(String shm_handle) throws Exception
+	public long generic_advance_read_index(long count, int over)
 	{
-		///more error checking needed
-		File f=new File(shm_handle);
-		if(!f.exists() || !f.canWrite())
+		if(count==0) {return 0;}
+		long can_read_count;
+		long do_advance_count;
+
+		if(over==1)
 		{
-			throw new Exception("ringbuffer "+shm_handle+" not found or not writable.");
+			can_read_count=can_read();
+			//limit to whole buffer
+			do_advance_count=Math.min(size(),count);
 		}
-		/*
-		"rws" Open for reading and writing, as with "rw", and also require that every 
-		update to the file's content or metadata be written synchronously to the 
-		underlying storage device. 
-		*/
-		RandomAccessFile in = new RandomAccessFile(f, "rws");
-		FileChannel fc = in.getChannel();
+		else
+		{
+			can_read_count=can_read();
+			if(can_read_count<1)
+			{
+				total_underflows(total_underflows()+1);
+				return 0;
+			}
+			do_advance_count=count > can_read_count ? can_read_count : count;
+		}
 
-		p("filechannel size bytes: "+fc.size()+" file size bytes "+f.length());
+		long r=read_index();
+		long linear_end=r+do_advance_count;
+		long tmp_read_index=linear_end>size() ? linear_end-size() : r+do_advance_count;
 
-		//map whole file
-		mbb=fc.map(FileChannel.MapMode.READ_WRITE,0,f.length());
-		mbb.order(ByteOrder.LITTLE_ENDIAN);
-		return fc.size();
-	}
+		read_index( tmp_read_index%=size() );
+
+		//if write index was overpassed, move up to read index
+		if(over==1 && can_read_count<do_advance_count)
+		{
+			write_index( read_index() );
+		}
+
+		last_was_write(0);
+
+		total_bytes_read(total_bytes_read()+do_advance_count);
+
+		if(do_advance_count<count){total_underflows(total_underflows()+1);}
+
+		return do_advance_count;
+	}//generic_advance_read_index()
 
 //=============================================================================
 	public void free() throws Exception
